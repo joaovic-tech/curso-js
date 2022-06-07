32740ac8a647862ac8eaabf993a74fb3b37aa25b
João <joaovictorca2004@gmail.com>
2022-06-07 12:02:23 -0300

    aula 31

---
+++ Aula-31-desestruturação-array/script.js
@@ -0,0 +1,6 @@
+// ... rest, ... spread
+//                    0          1          2
+//                 0  1  2    0  1  2    0  1  2
+const numbers = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+const [lista1, lista2, lista3] = numbers;
+console.log(lista3[2]);
