5c702bf8ebd5d3a1938656a68ec2e1a049efa339
João Victor <joaovictorca2004@gmail.com>
2022-12-14 15:10:35 -0300

    Aula da seção - api_rest

---
+++ api_rest/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': 0,
+  },
+};
