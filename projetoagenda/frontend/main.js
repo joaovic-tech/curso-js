db4f803965ad339db6e67fc19c5d59ef79d382fe
joaovic <joaovictorca2004@gmail.com>
2024-09-18 22:03:52 -0300

    Validando o frontend (desafio) aula 159

--- projetoagenda/frontend/main.js
+++ projetoagenda/frontend/main.js
@@ -1,6 +1,12 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import Login from './modules/Login';
+import Contato from './modules/Contato';
 
-import './assets/css/style.css';
+const login = new Login("form-login");
+const cadastro = new Login("form-cadastro-contato");
+const contato = new Contato("form-cadastro-contato");
 
-console.log('Olá mundo 3');
+login.init();
+cadastro.init();
+contato.init();
