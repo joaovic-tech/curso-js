0b773f05c34ec620569823d92bfdd90f0a779be9
joaovic <joaovictorca2004@gmail.com>
2024-09-24 22:32:39 -0300

    estruturando o projeto `api_rest`

---
+++ api_rest/src/routes/homeRoutes.js
@@ -0,0 +1,10 @@
+import {
+  Router,
+} from 'express';
+import HomeController from '../controllers/HomeController';
+
+const router = Router();
+
+router.get('/', HomeController.index);
+
+export default router;
