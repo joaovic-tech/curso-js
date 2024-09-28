b5a1515c5152c1f8066768acf00efc7bb5ad6838
joaovic <joaovictorca2004@gmail.com>
2024-09-28 17:58:33 -0300

    tratando os arquivos/uploads enviados

--- api_rest/src/config/multerConfig.js
+++ api_rest/src/config/multerConfig.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * 20000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/webp') {
+      return cb(new multer.MulterError('Tipo do arquivo inválido'));
+    }
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
--- api_rest/src/routes/fotoRoutes.js
+++ api_rest/src/routes/fotoRoutes.js
@@ -1,14 +1,10 @@
 import {
   Router,
 } from 'express';
-import multer from 'multer';
 import FotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = Router();
 
-router.post('/', upload.single('foto'), FotoController.store);
+router.post('/', FotoController.store);
 
 export default router;
