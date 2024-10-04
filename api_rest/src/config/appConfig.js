3e7b8b483406516ae836b4f443d965c65140d950
joaovic <joaovictorca2004@gmail.com>
2024-10-03 21:37:13 -0300

    Alterações no curso-js

--- api_rest/src/config/appConfig.js
+++
@@ -1,3 +0,0 @@
-export default {
-  url: process.env.APP_URL_DEV ? process.env.APP_URL_DEV : process.env.APP_URL,
-};
--- api_rest/src/server.js
+++
@@ -1,5 +0,0 @@
-import app from './app';
-
-app.listen(process.env.APP_PORT, () => {
-  console.log(`api_rest: ${process.env.APP_URL_DEV ? process.env.APP_URL_DEV : process.env.APP_URL}`);
-});
