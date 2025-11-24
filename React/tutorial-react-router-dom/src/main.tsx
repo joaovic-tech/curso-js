d8377d84d513b0b3198192fbb9e68af050ceaadc
joaovic <joaovictorca2004@gmail.com>
2025-11-24 17:10:22 -0300

    245. Rota 404: Rota pega tudo que não existe

--- React/tutorial-react-router-dom/src/main.tsx
+++ React/tutorial-react-router-dom/src/main.tsx
@@ -6,6 +6,8 @@ import { About } from "./components/About";
 import { Post } from "./components/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Menu } from "./components/menu";
+import { Redirect } from "./components/Redirect";
+import { NotFound } from "./components/NotFound";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,6 +18,8 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/about" element={<About />} />
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/posts" element={<Post />} />
+        <Route path="/redirect" element={<Redirect />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
