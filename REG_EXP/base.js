c4b2b0a8460b09dad0b18301437676538f974fcc
joaovic <joaovictorca2004@gmail.com>
2025-11-27 17:38:58 -0300

    aula 9 lookahead

--- REG_EXP/base.js
+++ REG_EXP/base.js
@@ -62,6 +62,15 @@ Os Ips são:
 
 `;
 
+const lookahead = `
+ONLINE  192.168.0.1 ABCDEF inactive
+OFFLINE  192.168.0.2 ABCDEF active
+ONLINE  192.168.0.3 ABCDEF active
+ONLINE  192.168.0.4 ABCDEF active
+OFFLINE  192.168.0.5 ABCDEF active
+OFFLINE  192.168.0.6 ABCDEF inactive
+`;
+
 module.exports = {
   texto,
   arquivos,
@@ -71,4 +80,5 @@ module.exports = {
   cpfs,
   cpfs2,
   ips,
+  lookahead
 };
