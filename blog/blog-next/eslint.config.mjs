8f45966474e552b635c1f950df0f8ac4147bf544
joaovic <joaovictorca2004@gmail.com>
2026-02-23 16:27:18 -0300

    chore(setup): initialize Next.js project
    Initial structure for the blog-next project including TypeScript, ESLint, and Prettier configurations.

---
+++ blog/blog-next/eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  eslintPluginPrettierRecommended,
+]);
